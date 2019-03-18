64742bc2ee1839012194c5bdc74e6586e3d61ba6
Saranpat Prasertthum <Saranpatp@gmail.com>
2019-03-18 22:08:03 +0700

    add Login

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 
-export default class Login extends Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <button>login</button>
-        </form>
-      </div>
-    );
-  }
-}
+const FormPage = () => {
+  return (
+    <MDBContainer >
+      <MDBRow>
+        <MDBCol md="11" >
+          <form>
+            <p className="h4 text-center mb-4">ORKD</p>
+            <label htmlFor="Username" className="grey-text">
+              Your Username
+            </label>
+            <input
+              type="Username"
+              id="defaultFormLoginUsername"
+              className="form-control"
+            />
+            <br />
+            <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+              Your password
+            </label>
+            <input
+              type="password"
+              id="defaultFormLoginPasswordEx"
+              className="form-control"
+            />
+            <div className="text-center mt-4">
+              <MDBBtn color="indigo" type="submit">Login</MDBBtn>
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
+
+export default FormPage;
