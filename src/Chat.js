d3ecdd15bb2188227deca8f1e3c2a90ec26119ef
Thanunya <b.beemmps@gmail.com>
2019-03-19 16:33:34 +0700

    chat

--- src/Chat.js
+++ src/Chat.js
@@ -3,6 +3,12 @@ import "./App.css";
 import Client from "./component/Client";
 
 export default class Chat extends Component {
+  componentWillMount() {
+    console.log("componentWillMount");
+  }
+
+  //   var socket = io.connect('http://localohost:3000');
+
   render() {
     return (
       <div className="boxChat">
