981f105820f6ae4b2b062de1ca4466eaff88b8cb
Thanunya <b.beemmps@gmail.com>
2019-03-19 10:46:14 +0700

    start chat page

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import "./App.css";
+import Client from "./component/Client";
 
 export default class Chat extends Component {
   render() {
-    return <div>chat12312</div>;
+    return (
+      <div className="boxChat">
+        <div className="block-left">
+          <Client />
+        </div>
+        <div className="block-right">2</div>
+      </div>
+    );
   }
 }
---
+++ src/component/Client.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class Client extends Component {
+  render() {
+    return (
+      <div>
+        <div className="header"> Chat </div>
+      </div>
+    );
+  }
+}
