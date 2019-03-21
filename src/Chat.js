779eb3d0a712db4e7a6008854702840ad0c8e070
Saranpat Prasertthum <Saranpatp@gmail.com>
2019-03-21 12:17:52 +0700

    Fix ghost chat and username
    Fix Job only username when submit message

--- src/Chat.js
+++ src/Chat.js
@@ -19,56 +19,7 @@ export default class Chat extends Component {
       userID: "untitle",
       grName: "GrUntitle",
       isCreate: false,
-      chats: [
-        {
-          username: "Job",
-          content: <p>วัดดีคับ</p>,
-          img:
-            "https://scontent.fbkk12-3.fna.fbcdn.net/v/t1.0-9/38737612_1929131837146363_6837282696201240576_n.jpg?_nc_cat=102&_nc_eui2=AeHAXKjNSVqYsLNyPhzu6AKUMyIujgbE12osK6lhugNB8dM6XcR2UJN6bwdZqT_2eneGnpy6CQsmlSEfeSd5r6n2OnBmX4MLNz6cDIQeVKcMVg&_nc_ht=scontent.fbkk12-3.fna&oh=0160a70904cd1c4afb3e7a315f7ccacd&oe=5D11651E"
-        },
-        {
-          username: "Zen",
-          content: <p>วัดกะกูได้</p>,
-          img:
-            "https://scontent.fbkk9-2.fna.fbcdn.net/v/t1.0-9/38817975_1983880531680485_703781742078590976_n.jpg?_nc_cat=109&_nc_eui2=AeES5xipaUyJQK0QaOChyO1AZOCmHBNkRgTcmc-3wpTisGPh0g-q_gDtklAstKKkv2d7sga4IyRMNr--uiYnsge7PuyGYVt4TbAddc3CBzrX5A&_nc_ht=scontent.fbkk9-2.fna&oh=ab36e66f7a88169d8f252d37bcc6e646&oe=5D036573"
-        },
-        {
-          username: "Patja",
-          content: <p>จัดเลยๆ</p>,
-          img:
-            "https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png"
-        },
-        {
-          username: "Most",
-          content: <p>ใจเยนๆนะ</p>,
-          img:
-            "https://scontent.fbkk9-2.fna.fbcdn.net/v/t1.0-9/12647440_1113114828700184_2077136073385067896_n.jpg?_nc_cat=109&_nc_eui2=AeHugNI61Z-GOiuz0EueA5kdpjfokLsTLDUKGNI3bv3IQ0irzeTCetUcVa_vaY5z1XDrCT0zeRCJ6tglql_J16ZXPHx8FU_BUcYQa8jZIGnFkA&_nc_ht=scontent.fbkk9-2.fna&oh=b2733fd429ef430f4c66ccc6d1ff87f5&oe=5D11D91B"
-        },
-        {
-          username: "Job",
-          content: <p>หลังวัดได้นะคับ</p>,
-          img:
-            "https://scontent.fbkk12-3.fna.fbcdn.net/v/t1.0-9/38737612_1929131837146363_6837282696201240576_n.jpg?_nc_cat=102&_nc_eui2=AeHAXKjNSVqYsLNyPhzu6AKUMyIujgbE12osK6lhugNB8dM6XcR2UJN6bwdZqT_2eneGnpy6CQsmlSEfeSd5r6n2OnBmX4MLNz6cDIQeVKcMVg&_nc_ht=scontent.fbkk12-3.fna&oh=0160a70904cd1c4afb3e7a315f7ccacd&oe=5D11651E"
-        },
-        {
-          username: "Zen",
-          content: <p>หลังวัดก็มาดิคาบ</p>,
-          img:
-            "https://scontent.fbkk9-2.fna.fbcdn.net/v/t1.0-9/38817975_1983880531680485_703781742078590976_n.jpg?_nc_cat=109&_nc_eui2=AeES5xipaUyJQK0QaOChyO1AZOCmHBNkRgTcmc-3wpTisGPh0g-q_gDtklAstKKkv2d7sga4IyRMNr--uiYnsge7PuyGYVt4TbAddc3CBzrX5A&_nc_ht=scontent.fbkk9-2.fna&oh=ab36e66f7a88169d8f252d37bcc6e646&oe=5D036573"
-        },
-        {
-          username: "Zen",
-          content: <p>เดวเเว้นไปหา เเง้นนนนๆๆๆๆ</p>,
-          img:
-            "https://scontent.fbkk9-2.fna.fbcdn.net/v/t1.0-9/38817975_1983880531680485_703781742078590976_n.jpg?_nc_cat=109&_nc_eui2=AeES5xipaUyJQK0QaOChyO1AZOCmHBNkRgTcmc-3wpTisGPh0g-q_gDtklAstKKkv2d7sga4IyRMNr--uiYnsge7PuyGYVt4TbAddc3CBzrX5A&_nc_ht=scontent.fbkk9-2.fna&oh=ab36e66f7a88169d8f252d37bcc6e646&oe=5D036573"
-        },
-        {
-          username: "Beam",
-          content: <p>หืมมมมม</p>,
-          img:
-            "https://i.kym-cdn.com/entries/icons/original/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.png"
-        }
-      ]
+      chats: []
     };
     this.onSubmit = this.onSubmit.bind(this);
     this.submitMessage = this.submitMessage.bind(this);
@@ -140,7 +91,7 @@ export default class Chat extends Component {
         {
           chats: this.state.chats.concat([
             {
-              username: "Job",
+              username: this.state.userID,
               content: <p>{msg}</p>,
               img:
                 "https://scontent.fbkk12-3.fna.fbcdn.net/v/t1.0-9/38737612_1929131837146363_6837282696201240576_n.jpg?_nc_cat=102&_nc_eui2=AeHAXKjNSVqYsLNyPhzu6AKUMyIujgbE12osK6lhugNB8dM6XcR2UJN6bwdZqT_2eneGnpy6CQsmlSEfeSd5r6n2OnBmX4MLNz6cDIQeVKcMVg&_nc_ht=scontent.fbkk12-3.fna&oh=0160a70904cd1c4afb3e7a315f7ccacd&oe=5D11651E"
@@ -155,7 +106,7 @@ export default class Chat extends Component {
   };
 
   render() {
-    const username = "Job";
+    //const username = "Job"; เปลี่ยน เป็น this.state.userID
     const { chats } = this.state;
     return (
       <div className="boxChat">
@@ -265,7 +216,7 @@ export default class Chat extends Component {
             <h3>ChatRoom</h3>
             <ul className="chats" ref="chats">
               {chats.map(chat => (
-                <Message chat={chat} user={username} />
+                <Message chat={chat} user={this.state.userID} />
               ))}
             </ul>
             <form className="input" onSubmit={e => this.submitMessage(e)}>
@@ -281,7 +232,7 @@ export default class Chat extends Component {
             <h3>ChatRoom</h3>
             <ul className="chats" ref="chats">
               {chats.map(chat => (
-                <Message chat={chat} user={username} />
+                <Message chat={chat} user={this.state.userID} />
               ))}
             </ul>
             <form className="input" onSubmit={e => this.submitMessage(e)}>
